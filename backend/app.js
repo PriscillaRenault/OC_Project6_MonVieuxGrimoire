5fbf1f1902d4a9fce6f8d2667bf2f61d18ad9fe1
PriscillaRenault <priscilla.renault72460@gmail.com>
2024-10-15 15:52:11 +0200

    create controllers models ans routes for books

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,31 @@
 const express = require('express')
-
 const app = express()
+const mongoose = require('mongoose')
+const bookRoutes = require('./routes/book')
+
+mongoose
+  .connect(
+    'mongodb+srv://priscillarenault72460:Pillette72,@cluster0.arzrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+app.use(express.json())
 
 app.use((req, res, next) => {
-  console.log('Requête reçue')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
   next()
 })
 
-app.use((req, res, next) => {
-  res.status(201)
-  next()
-})
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' })
-  next()
-})
-
-app.use((req, res) => {
-  console.log('Réponse envoyée avec succès !')
-})
+app.use(express.json())
+app.use('/api/books', bookRoutes)
 
 module.exports = app
